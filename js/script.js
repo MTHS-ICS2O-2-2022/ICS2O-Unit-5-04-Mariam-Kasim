1d92e64bfc3645147f4a4e47ec6fc5284bfe0f85
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-05-07 04:13:25 +0000

    main

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Mariam Kasim All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mariam Kasim
+// Created on: May
 // This file contains the JS functions for index.html
+
+"use strict"
+
+  // Function to decide whether or not the user would be allowed into the movie theatre
+  function myButtonClicked() {
+  // input
+  const age = parseInt(document.getElementById("age").value);
+  const day = document.getElementById("day-of-the-week").value;
+  
+  // process
+  if (age >= 18 && day != "Monday" && day != "Tuesday" && day != "Wednesday" && day != "Thursday") {
+    // output
+    document.getElementById('answer').innerHTML = "You can go to the movies!";
+  }
+  else { age < 18 || day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday"
+    // output
+    document.getElementById('answer').innerHTML = "You cannot go to the movies!";
+  }
+}
